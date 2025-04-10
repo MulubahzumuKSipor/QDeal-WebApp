8020d545e1bd090f2db8eae00a8e4b20c9e51b1a
Mulubahzumu Kemmeh Sipor <sip23001@byui.edu>
2025-04-10 15:34:23 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        dashboard: resolve(__dirname, "src/product-page/index.html"),
+        product: resolve(__dirname, "src/product-page/index.html"),
       },
     },
   },
